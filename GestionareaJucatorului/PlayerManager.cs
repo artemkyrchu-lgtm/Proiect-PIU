f9dcb577c3ef9a28ba963ed60292084ad9a26cd9
Artem Kyrchu <artem.kyrchu@student.usv.ro>
2026-03-15 21:28:41 +0200

    push

---
+++ GestionareaJucatorului/PlayerManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Jucator;
+
+namespace GestionareaJucatorului
+
+{
+    public class PlayerManager
+    {
+        public List<Player> PlayerList = new List<Player>();
+        public void AddPlayer(Player newPlayer) { PlayerList.Add(newPlayer); }
+        public void RemovePlayer(Player DelPlayer) { PlayerList.Remove(DelPlayer); }
+        public Player FindPlayerById(int id) { return PlayerList.Find(p => p.Id == id); }
+        public List<Player> GetPlayerByRole(string role) { return PlayerList.FindAll(p => p.Role == role); }
+    }
+}
---
+++ Jucator/Player.cs
@@ -0,0 +1,17 @@
+﻿namespace Jucator
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Nickname { get; set; }
+        public string Hero { get; set; }
+        public string Role { get; set; }
+
+        public string PerformanceStatus { get; set; } // adica cat de bun joace jucatorul
+        public double GamesPlayed { get; set; }
+        public int DamageDealt { get; set; }
+        public int HealingDone { get; set; }
+        public int DamageTaken { get; set; }
+
+    }
+}
